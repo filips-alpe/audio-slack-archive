14c73adcf1be1238396b03888a3349c1c8cd2b06
kriss <kriss.pujats@gmail.com>
2020-04-10 21:49:10 +0300

    listAllPeers works

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,8 @@
 import {PeerServer} from "peer";
 
-const peerServer = PeerServer({port: 9000, path: '/myapp'});
+const peerServer = PeerServer({port: 9000, allow_discovery: true});
+
+peerServer.on('connection', conn => console.log(`connected ${conn.getId()}`));
+peerServer.on('disconnect', conn => console.log(`disconnect ${conn.getId()}`));
+console.log('Peer Server listening on port 9000');
+
