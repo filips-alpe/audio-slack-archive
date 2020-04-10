fb33ba3026bc78f93892f6f2cd8d02093cf6a340
Filips Alpe <filips.alpe@gmail.com>
2020-04-10 15:59:30 +0700

    Add more darkness

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from '@emotion/styled/macro';
+
+const AppContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  min-height: calc(var(--vh, 1vh) * 100);
+`;
+
+document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+window.addEventListener('resize', () => {
+  document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+});
+
+const Sidebar = styled.aside`
+  width: 100px;
+  max-width: 20vw;
+  background: #b2b2b2;
+`;
+
+const Content = styled.main`
+  flex: 1;
+  background: #2b2b2b;
+`;
 
 function App() {
-  return <main>Nothing here yet, keep reloading</main>;
+  return (
+    <AppContainer>
+      <Sidebar></Sidebar>
+      <Content></Content>
+    </AppContainer>
+  );
 }
 
 export default App;
