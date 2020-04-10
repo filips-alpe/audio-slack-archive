680ea3e68fd706757cf31d25479f3a7f67846073
Filips Alpe <filips.alpe@gmail.com>
2020-04-10 16:16:32 +0700

    Add some teams in the sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,12 +24,60 @@ const Content = styled.main`
   background: #2b2b2b;
 `;
 
+const TeamLink = styled.button<{ active: boolean }>`
+  width: 100%;
+  height: 100px;
+  max-height: 20vw;
+  padding: 10px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background: ${({ active }) => (active ? '#909090' : 'transparent')};
+  &:hover {
+    background: #a2a2a2;
+  }
+`;
+
+const TeamAvatar = styled.img`
+  max-width: 100%;
+`;
+
 function App() {
   return (
     <AppContainer>
-      <Sidebar></Sidebar>
+      <Sidebar>
+        <Teams />
+      </Sidebar>
       <Content></Content>
     </AppContainer>
+  );
+}
+
+function Teams() {
+  const teams = [
+    {
+      avatar:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/The_Boring_Company_Logo.svg/200px-The_Boring_Company_Logo.svg.png',
+      name: 'The Boring Company',
+    },
+    {
+      avatar: 'https://upload.wikimedia.org/wikipedia/commons/8/85/3M_CORPORATION.png',
+      name: '3M',
+    },
+    {
+      avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png',
+      name: 'NASA',
+    },
+  ];
+  const [activeTeam, setActiveTeam] = React.useState(teams[0].name);
+  return (
+    <>
+      {teams.map((t) => (
+        <TeamLink active={t.name === activeTeam} onClick={() => setActiveTeam(t.name)}>
+          <TeamAvatar src={t.avatar} alt={t.name} />
+        </TeamLink>
+      ))}
+    </>
   );
 }
 
