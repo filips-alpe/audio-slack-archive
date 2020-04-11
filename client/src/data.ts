4e672d3fe2a0d852ef25fdb9f286404f6bf81163
Filips Alpe <filips.alpe@gmail.com>
2020-04-11 20:52:43 +0700

    Reorder teams

--- client/src/data.ts
+++ client/src/data.ts
@@ -14,38 +14,6 @@ export type Team = { name: string; avatar: string; users: User[] };
 
 export const teams: Team[] = [
   {
-    avatar:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/The_Boring_Company_Logo.svg/200px-The_Boring_Company_Logo.svg.png',
-    name: 'The Boring Company',
-    users: [
-      {
-        id: '1',
-        name: 'Elon Musk',
-        avatar: 'https://i1.sndcdn.com/avatars-000343928089-mj2j87-t500x500.jpg',
-      },
-      {
-        id: '2',
-        name: 'Kim Collins',
-        avatar: 'https://randomuser.me/api/portraits/women/37.jpg',
-      },
-      {
-        id: '3',
-        name: 'Joseph Arnold',
-        avatar: 'https://randomuser.me/api/portraits/men/98.jpg',
-      },
-      {
-        id: '4',
-        name: 'Carolyn Frazier',
-        avatar: 'https://randomuser.me/api/portraits/women/7.jpg',
-      },
-      {
-        id: '5',
-        name: 'Yoda',
-        avatar: 'https://avatarfiles.alphacoders.com/125/125043.jpg',
-      },
-    ],
-  },
-  {
     avatar: 'https://upload.wikimedia.org/wikipedia/commons/8/85/3M_CORPORATION.png',
     name: '3M',
     users: [
@@ -123,6 +91,38 @@ export const teams: Team[] = [
         id: '19',
         name: 'Wesley Silva',
         avatar: 'https://randomuser.me/api/portraits/men/21.jpg',
+      },
+    ],
+  },
+  {
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/The_Boring_Company_Logo.svg/200px-The_Boring_Company_Logo.svg.png',
+    name: 'The Boring Company',
+    users: [
+      {
+        id: '1',
+        name: 'Elon Musk',
+        avatar: 'https://i1.sndcdn.com/avatars-000343928089-mj2j87-t500x500.jpg',
+      },
+      {
+        id: '2',
+        name: 'Kim Collins',
+        avatar: 'https://randomuser.me/api/portraits/women/37.jpg',
+      },
+      {
+        id: '3',
+        name: 'Joseph Arnold',
+        avatar: 'https://randomuser.me/api/portraits/men/98.jpg',
+      },
+      {
+        id: '4',
+        name: 'Carolyn Frazier',
+        avatar: 'https://randomuser.me/api/portraits/women/7.jpg',
+      },
+      {
+        id: '5',
+        name: 'Yoda',
+        avatar: 'https://avatarfiles.alphacoders.com/125/125043.jpg',
       },
     ],
   },
